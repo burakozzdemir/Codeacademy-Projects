fd8ac512b9edcd5737876dffa231ba62f871f856
burakozzdemir <95588996+burakozzdemir@users.noreply.github.com>
2022-03-30 14:38:22 +0300

    Dog Years Calculate

---
+++ javascript/dog-years/app.js
@@ -0,0 +1,13 @@
+const myAge = 29;
+
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+
+earlyYears *= 4;
+
+let myAgeInDogYears = laterYears + earlyYears;
+
+console.log(`My name is 'Burak'.I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
