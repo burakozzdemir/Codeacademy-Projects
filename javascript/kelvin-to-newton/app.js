e88de1c88ddb142a344f58b4ca5445821847fe3f
burakozzdemir <95588996+burakozzdemir@users.noreply.github.com>
2022-03-30 12:24:24 +0300

    Kelvin to newton calculate

---
+++ javascript/kelvin-to-newton/app.js
@@ -0,0 +1,20 @@
+ const kelvin = 293;
+
+ const celsius = kelvin - 273;
+
+ let fahrenheit = celsius * (9/5) + 32;
+
+ fahrenheit = Math.floor(fahrenheit);
+
+ console.log(`The temparature is ${fahrenheit} degrees fahrenheit.`);
+
+ let newton = celsius * (33/100);
+
+ newton = Mat.floor(newton);
+
+ console.log(`The temparature is ${newton} degrees newton`);
+
+
+
+
+
