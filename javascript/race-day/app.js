b997936965b1db559fd4a28f3500943c8312761f
burakozzdemir <95588996+burakozzdemir@users.noreply.github.com>
2022-03-31 16:33:50 +0300

    Race day

---
+++ javascript/race-day/app.js
@@ -0,0 +1,21 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+
+let early = true;
+
+let runnerAge = 25;
+
+if (early && runnerAge > 18) {
+   raceNumber += 1000;
+}
+if (early && runnerAge > 18) {
+   console.log(`They will race at 9:30 am,your race number is: ${raceNumber}.`);
+}
+else if(!early && runnerAge > 18) {
+    console.log(`They will race at 11:00 am,your race number is: ${raceNumber}.`);
+}
+else if(runnerAge < 18) {
+    console.log(`They will race at 12:30 am,your race number is: ${raceNumber}.`);
+}
+else {
+    console.log(`Please approach the registration desk.`);
+}
