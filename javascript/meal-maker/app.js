858ca70cf628c688e1d90afdb954e6b301be29c9
burakozzdemir <95588996+burakozzdemir@users.noreply.github.com>
2022-04-10 17:23:56 +0300

    Meal maker

---
+++ javascript/meal-maker/app.js
@@ -0,0 +1,25 @@
+const menu = {
+  _menu: (''),
+  _price: 0,
+  set meal(mealToCheck) {
+    if (typeof mealToCheck === "string") {
+      return this._meal === mealToCheck;
+    }
+  },
+  set price(priceToCheck) {
+    if (typeof priceToCheck === number) {
+      return this._price === priceToCheck;
+    }
+  },
+  get todaysSpecial() {
+    if (this._meal && this._price) {
+      return `Today’s Special is ${this._meal} for $${this._price}!`;
+    } else {
+      return "Meal or price was not set correctly!";
+    }
+  },
+};
+
+menu._meal = "Hamburger";
+menu._price = 7;
+console.log(menu.todaysSpecial);
